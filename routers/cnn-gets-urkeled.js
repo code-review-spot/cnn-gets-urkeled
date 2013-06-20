c9d0e54decba6953d83c89f15a77f0667cc11787
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-06-19 21:55:49 -0400

    adding in image sizes

--- routers/cnn-gets-urkeled.js
+++ routers/cnn-gets-urkeled.js
@@ -2,7 +2,19 @@ var webshot = require("webshot");
 
 module.exports = function(app){
 	app.get("/cnn-gets-urkeled.png", function(req, res){		
-		webshot("http://cnn-gets-urkeled.herokuapp.com/urkel-and-headline", function(err, renderStream) {
+
+		var opt = {
+			screenSize: {
+    	width: 430,
+    	height: 352
+	  	},
+	  	shotSize: {
+	    	width: 430,
+	    	height: 'all'
+	  	}
+		}
+
+		webshot("http://cnn-gets-urkeled.herokuapp.com/urkel-and-headline", opt, function(err, renderStream) {
 			var img = "";
 
 			renderStream.on("data", function(data){
